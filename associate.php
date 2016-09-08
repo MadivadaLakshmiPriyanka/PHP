ededcd36a92af96e275583cf32c2847cc294d4b0
MadivadaLakshmiPriyanka <pinky.mlp@gmail.com>
2016-09-08 21:07:52 +0530

    Add files via upload

---
+++ associate.php
@@ -0,0 +1,7 @@
+<?php
+$descriptions=[
+'Earth'=>'mostly harmless',
+'Marvin'=>'the paranoid android'
+];
+$descriptions['Zaphod']='President';
+echo "Earth is described as {$descriptions['Earth']}";
