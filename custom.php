d2d8e4a5d1afc5639cfeca4187cc60085743ee8d
MadivadaLakshmiPriyanka <pinky.mlp@gmail.com>
2016-09-11 21:10:16 +0530

    Add files via upload

---
+++ custom.php
@@ -0,0 +1,12 @@
+<?php
+function lyn_convertToMinutes($seconds){
+$sec=$seconds%60;
+if(function_exists('intdiv')){
+$min=intdiv($seconds,60);
+}else{
+$min=($seconds-$sec)/60;
+}
+$sec=($sec<10)? '0' . $sec : $sec;
+return "$min:$sec";
+}
+echo lyn_convertToMinutes(547);
