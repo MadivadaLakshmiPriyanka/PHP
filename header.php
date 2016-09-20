b8a1aaf5ff4997d8232367d9acb2aa4581ba0516
MadivadaLakshmiPriyanka <pinky.mlp@gmail.com>
2016-09-20 18:20:45 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+<html>
+<head>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+<title>PHP</title>
+</head>
+<body>
+<div id="header">
+<div id="logo">
+<p>LOOO</p>
+</div>
+<div id="subscribe">
+<p>Header Subscribe</p>
+</div>
+</div>
+<div id="navbar">
+<p>navbar</p>
+</div>
+</body>
+</html>
+?>
